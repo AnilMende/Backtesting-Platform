84edf812e1a5e3b58a3a71b67009c08b2f381acb
Anil <anilmende2002@gmail.com>
2026-06-22 01:18:02 +0530

    switched from SQLite to MySQL

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -1,12 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///backtesting.db"
+load_dotenv()
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
--- backend/app/models/company.py
+++ backend/app/models/company.py
@@ -2,16 +2,25 @@ from sqlalchemy import Column, Integer, String, BigInteger
 
 from app.database.base import Base
 
-
 class Company(Base):
     __tablename__ = "companies"
 
     id = Column(Integer, primary_key=True, index=True)
 
-    symbol = Column(String, unique=True, nullable=False)
+    symbol = Column(
+        String(50),
+        unique=True,
+        nullable=False
+    )
 
-    company_name = Column(String)
+    company_name = Column(
+        String(255)
+    )
 
-    sector = Column(String)
+    sector = Column(
+        String(255)
+    )
 
-    market_cap = Column(BigInteger)
+    market_cap = Column(
+        BigInteger
+    )
